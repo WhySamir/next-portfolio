63f0b6f7382e623324c01773cc75b1ba26c9d019
samir1611 <johnshakya16@gmail.com>
2025-07-15 20:20:24 +0545

    adjusted deployment for private folders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import LandingPage from "./components/LandingPage";
-import Skills from "./priv_components/Skills";
+// import Skills from "./priv_components/Skills";
+import { Skills } from "next-priv-components";
 
 export default function Home() {
   return (
