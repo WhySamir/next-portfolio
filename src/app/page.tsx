4deb38d80c4834c28c9850f24d71c25be1ce8b97
whysamir <csit23081025_samir@achsnepal.edu.np>
2025-07-17 00:49:21 +0545

    commited

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 import LandingPage from "./components/LandingPage";
-// import Skills from "./priv_components/Skills";
-
-import { Skills } from "next-priv-components";
-import Image from "next/image";
+import Skills from "./priv_components/Skills";
 
 export default function Home() {
   return (
@@ -11,17 +8,7 @@ export default function Home() {
       <LandingPage />
       <div className="flex flex-col px-5 sm:px-10 relative">
         <div className="max-w-7xl mx-auto w-full">
-          <Skills
-            ImageComponent={(props) => (
-              <Image
-                {...(props as React.ComponentProps<typeof Image>)}
-                width={32}
-                height={32}
-                quality={100}
-                loading="lazy"
-              />
-            )}
-          />
+          <Skills />
         </div>
       </div>
     </>
